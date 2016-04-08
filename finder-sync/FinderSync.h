32bfa5f16b1dc761a9a6dae59a0665e1268f4671
iburlakov <iburlakov@gmail.com>
2016-04-08 12:54:11 +0300

    Empty finder sync extension.

---
+++ finder-sync/FinderSync.h
@@ -0,0 +1,14 @@
+//
+//  FinderSync.h
+//  finder-sync
+//
+//  Created by Ivan Burlakov on 4/7/16.
+//  Copyright © 2016 Ivan Burlakov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <FinderSync/FinderSync.h>
+
+@interface FinderSync : FIFinderSync
+
+@end
